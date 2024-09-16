26a9d75c71f5b62e2980fb9ac4f3fcec31aa2f66
IgorMassaru <61235905+IgorMassaru@users.noreply.github.com>
2024-09-16 19:59:27 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ function pesquisar() {
     campoPesquisa = campoPesquisa.toLowerCase()
 
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
-    let resultados = "";
+   
+    let resultadosDado = "";
     let titulo = "";
     let descricao = "";
+   
 
     // Itera sobre cada item de dados e constrói o HTML para cada resultado
     for(let dado of dados) {
@@ -28,9 +30,9 @@ function pesquisar() {
         // se titulo ou descrição includes campoPesquisa
         if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
             
-            console.log(dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || tags.includes(campoPesquisa))
+            console.log(dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || dado.tags.includes(campoPesquisa))
             // Cria um novo elemento de resultado
-            resultados += `
+            resultadosDado += `
             <div class="item-resultado">
             <h2>${dado.titulo}</h2> 
             <p class="descricao-meta">${dado.descricao}</p> 
@@ -40,10 +42,84 @@ function pesquisar() {
         
     }
 
-    if(!resultados){
-        resultados = "Tecnologia não encontrada. Busque por uma Técnologia ou termo da área."
+    if(!resultadosDado){
+        resultadosDado = "Tecnologia não Encontrada. Busque por uma Tecnologia em alta em 2024."
     }
     // Atribui o HTML gerado à seção de resultados
-    section.innerHTML = resultados;
+    section.innerHTML = resultadosDado;
+    
 }
+
+function pesquisarEmpresa() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa-empresa");
+    
+    let campoPesquisaEmpresa = document.getElementById("campo-pesquisa-empresa").value
+    console.log(campoPesquisaEmpresa);
+    
+    // se campoPesquisa for uma string sem nada
+    if(campoPesquisaEmpresa == ""){
+        section.innerHTML = "Empresa não encontrada"
+        return 
+    }
+
+    campoPesquisaEmpresa = campoPesquisaEmpresa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultadosEmpresa = "";
+    let nome = "";
+    let descricao = "";
+    let ramoDeAtividade = "";
+    let fundadores = "";
+
+
+    for(let empresa of empresasInovadoras) {
+
+        nome = empresa.nome.toLowerCase()
+        ramoDeAtividade = empresa.ramoDeAtividade.toLowerCase()
+        descricao = empresa.descricao.toLowerCase()
+        fundadores = empresa.fundadores.toLowerCase()
+
+        // se titulo ou descrição includes campoPesquisa
+        if(nome.includes(campoPesquisaEmpresa) || ramoDeAtividade.includes(campoPesquisaEmpresa) || descricao.includes(campoPesquisaEmpresa) || fundadores.includes(campoPesquisaEmpresa)){
+            
+            console.log(empresa.nome.includes(campoPesquisaEmpresa) || empresa.ramoDeAtividade.includes(campoPesquisaEmpresa) || empresa.descricao.includes(campoPesquisaEmpresa) || empresa.fundadores.includes(campoPesquisaEmpresa))
+            // Cria um novo elemento de resultado
+            resultadosEmpresa += `
+            <div class="item-resultado">
+            <h2>${empresa.nome}</h2> 
+            <h2>${empresa.ramoDeAtividade}</h2>
+            <p class="descricao-meta">${empresa.descricao}</p>
+            <p class="descricao-meta">${empresa.fundadores}</p>
+            <a href=${empresa.link} target="_blank ">Mais Informações sobre a empresa</a> </div>
+            `;
+        }
+        
+    }
+
+    if(!resultadosEmpresa){
+        resultadosEmpresa = "Empresa não Encontrada. Busque por Empresas de Técnologia em alta em 2024."
+    }
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultadosEmpresa;
+
+    
+}
+
+const campoPesquisa = document.getElementById("campo-pesquisa");
+
+        campoPesquisa.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                pesquisar();
+            }
+        });
+
+
+const campoPesquisaEmpresa = document.getElementById("campo-pesquisa-empresa");
+
+        campoPesquisaEmpresa.addEventListener('keydown', function(event1) {
+            if (event1.key === 'Enter') {
+                pesquisarEmpresa();
+            }
+        });
 
--- dados.js
+++ dados.js
@@ -50,3 +50,96 @@ let dados = [
     }
 ];
 
+let empresasInovadoras = [
+    {
+      nome: "Nubank",
+      ramoDeAtividade: "Fintech",
+      descricao: "Uma das maiores fintechs da América Latina, oferecendo serviços financeiros inovadores.",
+      link: "https://nubank.com.br/",
+      fundadores: "David Vélez, Cristina Junqueira e Edward Waisman"
+    },
+    {
+      nome: "iFood",
+      ramoDeAtividade: "Tecnologia para restaurantes",
+      descricao: "A maior plataforma de delivery de comida da América Latina.",
+      link: "https://www.ifood.com.br/",
+      fundadores: "Moisés Sá, Guilherme Telles e Fabrício Blois"
+    },
+    {
+      nome: "Stone",
+      ramoDeAtividade: "Fintech",
+      descricao: "Uma das principais empresas de pagamentos do Brasil, oferecendo soluções para pequenos e médios negócios.",
+      link: "https://www.stone.com.br/",
+      fundadores: "Thiago Paguete, Renato Grinberg e Gabriel Braga"
+    },
+    {
+      nome: "Loggi",
+      ramoDeAtividade: "Logística",
+      descricao: "Plataforma de logística urbana que conecta empresas e consumidores através de uma rede de entregadores.",
+      link: "https://www.loggi.com/",
+      fundadores: "Deodoro Junior, Fabien Mendez, Fabien Echeverria"
+    },
+    {
+      nome: "QuintoAndar",
+      ramoDeAtividade: "Imóveis",
+      descricao: "Plataforma online para locação de imóveis residenciais e comerciais, oferecendo uma experiência mais simples e transparente.",
+      link: "https://www.quintoandar.com.br/",
+      fundadores: "Gabriel Braga, Guilherme Bueno, Hugo Rodrigues"
+    },
+    {
+        nome: "Wildlife Studios",
+        ramoDeAtividade: "Games",
+        descricao: "Uma das maiores desenvolvedoras de jogos mobile do mundo, com sede no Brasil.",
+        link: "https://www.wildlifestudios.com/",
+        fundadores: "Victor Boccato, Ricardo Chahine e Arthur Pires"
+    },
+    {
+        nome: "Konduto",
+        ramoDeAtividade: "Fintech",
+        descricao: "Plataforma de prevenção à fraude em pagamentos online, utilizada por grandes empresas.",
+        link: "https://konduto.com/",
+        fundadores: "Caio Kon, Rodrigo García e Thiago Alvarez"
+    },
+    {
+        nome: "Movile",
+        ramoDeAtividade: "Tecnologia",
+        descricao: "Holding de empresas de tecnologia com foco em serviços móveis, incluindo iFood.",
+        link: "https://www.movile.com/",
+        fundadores: "Fabricio Blois, Guilherme Telles, Moises Sá"
+    },
+    {
+        nome: "Sympla",
+        ramoDeAtividade: "Eventos",
+        descricao: "Plataforma para criação e venda de ingressos para eventos, com foco em eventos culturais e sociais.",
+        link: "https://www.sympla.com.br/",
+        fundadores: "Andre Banks, Bruno Lemos e Pedro Guth"
+    },
+    {
+        nome: "Creditas",
+        ramoDeAtividade: "Fintech",
+        descricao: "Plataforma que oferece soluções de crédito para consumidores, com foco em refinanciamento de dívidas.",
+        link: "https://www.creditas.com/",
+        fundadores: "Sergio Furio, Eduardo Gouveia e Thiago Alvarez"
+    },
+    {
+        nome: "Mercado Bitcoin",
+        ramoDeAtividade: "Criptomoedas",
+        descricao: "Uma das maiores corretoras de criptomoedas do Brasil.",
+        link: "https://www.mercadobitcoin.com.br/",
+        fundadores: "Reinaldo Rabelo e Pedro Moura"
+    },
+    {
+        nome: "Zenklub",
+        ramoDeAtividade: "Saúde mental",
+        descricao: "Plataforma que conecta pessoas a psicólogos online, oferecendo terapia e bem-estar mental.",
+        link: "https://www.zenklub.com/",
+        fundadores: "Não Identificado" 
+    },
+    {
+        nome: "Loft",
+        ramoDeAtividade: "Imóveis",
+        descricao: "Plataforma online para compra e venda de imóveis, oferecendo uma experiência digital e transparente.",
+        link: "https://www.loft.com.br/",
+        fundadores: "Daniel Serra, Guilherme Benincá e Lorival Bonfim"
+    }
+];
